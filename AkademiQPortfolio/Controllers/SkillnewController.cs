15d229fdf084d1ea431e00bd24dd134dcc5444af
SEDANUR YARDIMCI <gthjb>
2025-12-25 16:05:38 +0300

    17.12.2025

---
+++ AkademiQPortfolio/Controllers/SkillnewController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AkademiQPortfolio.Controllers
+{
+    public class SkillnewController : Controller
+    {
+        public IActionResult SkillList()
+        {
+
+            return View();
+        }
+    }
+}
